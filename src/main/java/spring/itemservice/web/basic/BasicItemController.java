d9a7842f1938d4b0ce20c2d925493e3f49aa2a50
mjkim856 <mjkim856@gmail.com>
2023-02-05 22:22:57 +0900

    feat: BasicItemController 생성

---
+++ src/main/java/spring/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,38 @@
+package spring.itemservice.web.basic;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.itemservice.domain.Item;
+import spring.itemservice.domain.ItemRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/item";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("item1", 10000, 10));
+        itemRepository.save(new Item("item2", 20000, 20));
+
+    }
+}
