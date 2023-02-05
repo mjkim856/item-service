b74faac6d850769fbbe6028f3c11718f985824a9
mjkim856 <mjkim856@gmail.com>
2023-02-06 01:19:59 +0900

    feat: 상품 upload html / 타임리프 구현

--- src/main/java/spring/itemservice/web/basic/BasicItemController.java
+++ src/main/java/spring/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.itemservice.domain.Item;
 import spring.itemservice.domain.ItemRepository;
@@ -36,6 +37,11 @@ public class BasicItemController {
 
     @GetMapping("/add")
     public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
         return "basic/addForm";
     }
 
