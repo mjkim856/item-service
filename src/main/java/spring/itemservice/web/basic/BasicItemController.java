249037cb1bd587026386c0daee551fd122dc6d06
mjkim856 <mjkim856@gmail.com>
2023-02-06 01:52:37 +0900

    feat: 상품 등록 controller 구현

--- src/main/java/spring/itemservice/web/basic/BasicItemController.java
+++ src/main/java/spring/itemservice/web/basic/BasicItemController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import spring.itemservice.domain.Item;
 import spring.itemservice.domain.ItemRepository;
 
@@ -40,9 +37,42 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+//    @PostMapping("/add")
+    public String addItem1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setQuantity(quantity);
+        item.setPrice(price);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItem2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+        // model.addAttribute("item", item); 자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItem3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String addItem4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
